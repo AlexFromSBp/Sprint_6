bce60480aa9c53b26f8bfc39be1ce6823ee22760
AlexFromSBp <tribunaleksandra@gmail.com>
2025-03-27 23:12:26 +0300

    Add files via upload

---
+++ helper/urls.py
@@ -0,0 +1,8 @@
+class Urls:
+    dzen_domain = 'dzen.ru'
+    yandex_search = 'ya.ru'
+    yandex_domain = 'yandex.ru'
+    base_url = 'https://qa-scooter.praktikum-services.ru'
+    main_page = base_url + '/'
+    order_page = base_url + '/order'
+    order_status_page = base_url + '/track'
